5ecc080078021f1523da9d0a7d52cc95287bda27
Supermjork <markehabaziz@gmail.com>
2023-10-30 17:45:57 +0200

    Some work on Puzzle class

--- slidey.py
+++ slidey.py
@@ -1,4 +1,24 @@
-# Folder containing Puzzle class
+# File containing Puzzle class
+
+# Goal:
+# 0 1 2
+# 3 4 5
+# 6 7 8
 class Puzzle:
-    def __init__(self) -> None:
-        pass
+    goal = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    state = []
+
+    def __init__(self, state):
+        self.state = state
+
+    def where_blank(self):
+        for i in range(len(self.state)):
+            for j in range(len(self.state[i])):
+                if self.state[i][j] == 0:
+                    return (i, j)
+        print("0 Not Found")
+
+    def where_blank_gen(self):
+        return [(i, j) for i in range(len(self.state)) for j in range(len(self.state[i])) if self.state[i][j] == 0]
+
+test_puzzle = Puzzle([[1, 2, 3], [0, 7, 5], [8, 6, 4]])
